461c5cd4dfa40a258b0b1786cc18c762c7ba1fbe
Caue Prado <caue.prado0@gmail.com>
2023-09-18 06:34:00 -0300

    Aula 1.10

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +15,13 @@ public class ScreenmatchApplication implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         ConsumoApi consumoApi = new ConsumoApi();
-        var json = consumoApi.obterDados("http://www.omdbapi.com/?t=gilmore+girls&Season=1&apikey=11aea8f0");
+        var json = consumoApi.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=11aea8f0");
         System.out.println(json);
+
+        ConverteDados conversor = new ConverteDados();
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
     }
 }
